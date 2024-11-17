edd6e8b40ea24566c4cb1e534f276824aa073410
ramo2004 <omarimian@gmail.com>
2024-11-16 19:10:08 -0600

    Added database schema for project setup

---
+++ schema.sql
@@ -0,0 +1,158 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.0 (Postgres.app)
+-- Dumped by pg_dump version 17.0 (Postgres.app)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: Stocks; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Stocks" (
+    stock_id integer NOT NULL,
+    stock_ticker character varying DEFAULT ''::character varying NOT NULL,
+    company_name character varying DEFAULT ''::character varying NOT NULL,
+    current_price numeric DEFAULT 0.0 NOT NULL,
+    sector character varying DEFAULT ''::character varying NOT NULL
+);
+
+
+ALTER TABLE public."Stocks" OWNER TO postgres;
+
+--
+-- Name: Stocks_stock_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public."Stocks_stock_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public."Stocks_stock_id_seq" OWNER TO postgres;
+
+--
+-- Name: Stocks_stock_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public."Stocks_stock_id_seq" OWNED BY public."Stocks".stock_id;
+
+
+--
+-- Name: Users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Users" (
+    user_id integer NOT NULL,
+    first_name character varying DEFAULT ''::character varying NOT NULL,
+    last_name character varying DEFAULT ''::character varying NOT NULL,
+    user_name character varying DEFAULT ''::character varying NOT NULL,
+    password character varying DEFAULT ''::character varying NOT NULL,
+    email character varying DEFAULT ''::character varying NOT NULL,
+    security_question character varying(255),
+    security_answer character varying(255)
+);
+
+
+ALTER TABLE public."Users" OWNER TO postgres;
+
+--
+-- Name: Users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public."Users_user_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public."Users_user_id_seq" OWNER TO postgres;
+
+--
+-- Name: Users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public."Users_user_id_seq" OWNED BY public."Users".user_id;
+
+
+--
+-- Name: Stocks stock_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Stocks" ALTER COLUMN stock_id SET DEFAULT nextval('public."Stocks_stock_id_seq"'::regclass);
+
+
+--
+-- Name: Users user_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Users" ALTER COLUMN user_id SET DEFAULT nextval('public."Users_user_id_seq"'::regclass);
+
+
+--
+-- Name: Stocks Stocks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Stocks"
+    ADD CONSTRAINT "Stocks_pkey" PRIMARY KEY (stock_id);
+
+
+--
+-- Name: Users Users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Users"
+    ADD CONSTRAINT "Users_pkey" PRIMARY KEY (user_id);
+
+
+--
+-- Name: Users unique_email; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Users"
+    ADD CONSTRAINT unique_email UNIQUE (email);
+
+
+--
+-- Name: Stocks unique_stock_ticker; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Stocks"
+    ADD CONSTRAINT unique_stock_ticker UNIQUE (stock_ticker);
+
+
+--
+-- Name: Users unique_username; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Users"
+    ADD CONSTRAINT unique_username UNIQUE (user_name);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
