1236b63858c1941c9e2a5142ccc84318b15d9165
tibianelsheikh <110976066+tibianelsheikh@users.noreply.github.com>
2024-11-17 16:52:05 -0600

    stockdash logo, font adjustments

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import './LoginPage.css';
 import { Link } from 'react-router-dom';
+import logo from './logo.png';  // Import the logo image
 
 function LoginPage() {
   return (
     <div className="wrapper">
-      <form action="/register"> {/* Action should ideally be replaced by React Router for navigation */}
+      <div className="header">
+        {/* Logo */}
+        <img src={logo} alt="Stock Dash Logo" className="logo" />
+
+        {/* Title and subtitle */}
         <h2>Stock Dash</h2>
         <h3>All your stocks in one place.</h3>
+      </div>
 
-        {/* Email input field */}
+      {/* Login form */}
+      <form action="/register"> 
         <div className="input-field">
           <input type="text" required />
           <label>Enter your email</label>
         </div>
 
-        {/* Password input field */}
         <div className="input-field">
           <input type="password" required />
           <label>Enter your password</label>
         </div>
 
-
-        {/* Login button */}
         <button type="submit">Log In</button>
 
-        {/* Register link (using React Router's Link component) */}
         <div className="register">
           <p>
             Don't have an account? <Link to="/">Register</Link>
